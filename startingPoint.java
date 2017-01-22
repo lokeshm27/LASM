0cc01cde31f9908183caa6757a670d18eccd1df0
Lokesh M <lokeshm27@gmail.com>
2017-01-22 11:33:59 +0530

    startingPoint.prepare : reading file complete

--- startingPoint.java
+++ startingPoint.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -8,6 +13,8 @@ public class startingPoint {
 	static FileHandler handler;
 	static MyFormatter formatter = new MyFormatter();
 	static HashMap<Integer, String[]> program = new HashMap<Integer, String[]>();
+	static int[] locCtr = {0x000, 0x000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000};
+	static int maxBlocks = 10;
 
 	public static void main(String args[]) {
 		try {
@@ -15,7 +22,7 @@ public class startingPoint {
 			handler.setFormatter(formatter);
 			logger.addHandler(handler);
 			logger.setUseParentHandlers(false);
-			logger.info("lasm.statingPoint.main : Logger initialized.");
+			logger.info("lasm.startingPoint.main : Logger initialized.");
 		} catch (Exception e) {
 			logger.warning("lasm.statingPoint.main : Exception : " + e.getMessage());
 			logger.log(Level.WARNING, "Stack Trace : ", e );
@@ -30,18 +37,65 @@ public class startingPoint {
 		for(String fileName : args){
 			logger.info("lasm.startingPoint.main : Processing file : " + fileName);
 			prepare(fileName);
-			passOne(fileName);
-			passTwo(fileName);
 			program = new HashMap<Integer, String[]>();
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	static public void prepare(String fileName){
-		////TODO
+		File file = new File(fileName);
+		String line;
+		int programLenght = 0x0000;
+		int lineNum = 0;
+		try {
+			logger.info("lasm.startingPoint.prepare : Reading input file : " + fileName);
+			BufferedReader buff = new BufferedReader(new FileReader(file));
+			while((line = buff.readLine()) != null){
+				lineNum++;
+				line = line.replaceAll("\\s+"," ");
+				if(line.startsWith(";") || line.startsWith("\\t*\\s*;")){
+					logger.info("lasm.startingPoint.prepare : line " + Integer.toString(lineNum) + " : Comment Line. Skipping.");
+					continue;
+				}
+				String fields[] = line.split(" ");
+				switch(fields.length){
+					case 0 : 
+						logger.info("lasm.startingPoint.prepare : line " + Integer.toString(lineNum) + ": Empty line.");
+						break;
+					
+					case 1 : 
+						////TODO
+						break;
+						
+					case 2 : 
+						////TODO
+						break;
+						
+					case 3 :
+						////TODO
+						break;
+						
+					default :
+				}
+			}
+		} catch (FileNotFoundException e) {
+			logger.severe("lasm.startingPoint.prepare : input file : " + fileName + " doesn't exist");
+			System.out.println("Error : Input file not found.");
+			return;
+		} catch (IOException e) {
+			logger.severe("lasm.startingPoint.prepare : IOException while reading file : " + fileName);
+			logger.log(Level.SEVERE, "Stack Trace : ", e);
+			System.out.println("Error : Can't read input file : " + fileName);
+			return;
+		}
+		
+		
+		//passOne(fileName);
 	}
 	
 	static public void passOne(String fileName){
 		////TODO
+		//passTwo(fileName);
 	}
 	
 	static public void passTwo(String fileName){
