3ef487c2406d334baee5793ff10fe7982c8780b5
Lokesh M <lokeshm27@gmail.com>
2017-01-22 09:41:56 +0530

    First Commit

---
+++ startingPoint.java
@@ -0,0 +1,7 @@
+import java.util.logging.Logger;
+
+public class startingPoint {
+	static Logger logger = Logger.getLogger("lasm.startingPoint");
+	static FileHandler handler;
+	static MyFormatter formatter = new MyFormatter();
+}
